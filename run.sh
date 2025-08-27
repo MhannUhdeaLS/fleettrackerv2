be1642a95f8c9e02c685b673da8d2a1fcdad5d7a
MhannUhdeaLS <111077106+MhannUhdeaLS@users.noreply.github.com>
2025-08-27 12:23:53 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nerdctl run -d --name fleettrackerv2 -e ALLY_CODES_URL=http://rotbot.eu/sniper/ac-1007299187345477712-fleet.json -e ARENA_TYPE=FLEET -e DISCORD_WEB_HOOK=https://discord.com/api/webhooks/1407317124120313958/9xtiZtUetehXPJVA894_t8MFXX458s0jdb0IuKmtMNaZIIdTBSB45XvQ0WXVYYs6lb95 --net=host --restart unless-stopped swgoh-arena-tracker:latest
+sudo nerdctl run -d --name fleettrackerv2 -e ALLY_CODES_URL=http://rotbot.eu/sniper/ac-1007299187345477712-fleet.json -e ARENA_TYPE=FLEET -e DISCORD_WEB_HOOK=https://discord.com/api/webhooks/1407317124120313958/9xtiZtUetehXPJVA894_t8MFXX458s0jdb0IuKmtMNaZIIdTBSB45XvQ0WXVYYs6lb95 --net=host --restart unless-stopped fleettrackerv2:latest
